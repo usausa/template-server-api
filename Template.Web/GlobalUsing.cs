972a9e01e19e3a955879b1112af0f9c0e25525c9
usausa <machi.pon@gmail.com>
2022-02-09 16:29:30 +0900

    Update global using

---
+++ Template.Web/GlobalUsing.cs
@@ -0,0 +1,17 @@
+// ReSharper disable RedundantUsingDirective.Global
+#pragma warning disable
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.ComponentModel.DataAnnotations;
+global using System.Data.Common;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Globalization;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+
+global using Microsoft.Extensions.Logging;
