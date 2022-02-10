ca302503b16459a9790a4730ce3ca562b7597813
usausa <machi.pon@gmail.com>
2022-02-10 16:50:53 +0900

    Update global using

--- Template.Web/GlobalUsing.cs
+++ Template.Web/GlobalUsing.cs
@@ -16,3 +16,7 @@ global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
 
 global using Microsoft.Extensions.Logging;
+
+global using Smart;
+global using Smart.Collections.Generic;
+global using Smart.Text;
