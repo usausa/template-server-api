02860efa681b178c8fe38c72cc00ebc93d2b09e1
usausa <machi.pon@gmail.com>
2022-11-21 16:42:50 +0900

    Update setting

--- Template.Web/Program.cs
+++ Template.Web/Program.cs
@@ -30,11 +30,8 @@ builder.Host
     .UseSystemd();
 
 // Log
+builder.Logging.ClearProviders();
 builder.Host
-    .ConfigureLogging((_, logging) =>
-    {
-        logging.ClearProviders();
-    })
     .UseSerilog((hostingContext, loggerConfiguration) =>
     {
         loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
